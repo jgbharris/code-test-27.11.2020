7cc263cc77c7735b416ad0003ffab7c52768ab74
Your Name <jgbharris@hotmail.co.uk>
2020-12-01 23:33:08 +0000

    API call working and value from RD:647 keys coming through

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+/* eslint-disable no-unused-expressions */
+
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://reference.intellisense.io/thickenernn/v1/referencia")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+
+console.log(data)
+data.current ? console.log(data.current.data["pt2-scaled"]) : null;
+
+let object = data.current ? data.current.data["pt2-scaled"] : null;
+
+for (const property in object) {
+  if(!property.startsWith('RD:647')) continue;
+  console.log(`${property}: ${object[property]["times"]}`);
+  console.log(object[property]["times"]);
+  console.log(`${property}: ${object[property]["values"]}`);
+  console.log(object[property]["values"]);
+
+  
+  
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
