2f1cfabb70a13451ac6ac731be9f871edf01dcc2
Your Name <jgbharris@hotmail.co.uk>
2020-12-03 10:55:39 +0000

    Webdatarocks table rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 import logo from './logo.svg';
 import './App.css';
 /* eslint-disable no-unused-expressions */
@@ -13,39 +15,43 @@ function App() {
       .then((data) => setData(data));
   }, []);
 
+  const options = {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: 'My chart'
+    },
+    series: [
+      {
+        data: [1, 2, 1, 4, 3, 6]
+      }
+    ]
+  };
 
-console.log(data)
-data.current ? console.log(data.current.data["pt2-scaled"]) : null;
 
-let object = data.current ? data.current.data["pt2-scaled"] : null;
+  console.log(data)
+  data.current ? console.log(data.current.data["pt2-scaled"]) : null;
 
-for (const property in object) {
-  if(!property.startsWith('RD:647')) continue;
-  console.log(`${property}: ${object[property]["times"]}`);
-  console.log(object[property]["times"]);
-  console.log(`${property}: ${object[property]["values"]}`);
-  console.log(object[property]["values"]);
+  let object = data.current ? data.current.data["pt2-scaled"] : null;
 
-  
-  
-}
+  for (const property in object) {
+    if (!property.startsWith('RD:647')) continue;
+    // console.log(`${property}: ${object[property]["times"]}`);
+    console.log(`${property}`, object[property]["times"]);
+    // console.log(`${property}: ${object[property]["values"]}`);
+    console.log(`${property}`, object[property]["values"]);
+  }
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+       <div>
+    </div>
+      <div id="wdr-component"></div>
+        <HighchartsReact highcharts={Highcharts} options={options} />
       </header>
     </div>
   );
